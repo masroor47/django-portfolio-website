df92c030e56b212ca60096e7104886f7db68ba35
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-02-22 21:59:39 -0500

    added redirect to the job details page if you click that particular job on homepage

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home'),
-    path('jobs/<int:job_id>', views.detail),
+    path('jobs/<int:job_id>', views.detail, name='detail'),
 ]
