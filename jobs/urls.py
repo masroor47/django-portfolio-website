84cc8b06efe0dd71c0998629e47088e1e58b6bf8
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-02-22 21:52:13 -0500

    added html and styling to the job details page, added job photo, and back button

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage),
+    path('', views.homepage, name='home'),
     path('jobs/<int:job_id>', views.detail),
 ]
